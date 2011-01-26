60f6d8c6d3e06ffd1be57e93ee7fd381c00de70e
troelslenda <troels+git@reload.dk>
2011-01-26 16:12:39 +0100

    forgot type for type event

--- node-event.tpl.php
+++ node-event.tpl.php
@@ -52,6 +52,9 @@
 
 
 ?>
+<h1><?php print t($node->type)?></h1>
+
+
 <?php print theme('imagecache','mobile-list-image',$node->field_list_image[0]['filepath']); ?>
 <div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> clear-block">
 
