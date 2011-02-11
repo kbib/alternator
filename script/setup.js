0e256fc4f539f8b70667542bc6641bb192a077c3
troelslenda <troels+git@reload.dk>
2011-02-11 14:09:14 +0100

    adding fix for label clicks for iphone

--- script/setup.js
+++ script/setup.js
@@ -1,5 +1,18 @@
 $(function(){
 
+	if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i)) {
+		$(document).ready(function () {
+			$('label[for]').click(function () {
+				var el = $(this).attr('for');
+				if ($('#' + el + '[type=radio], #' + el + '[type=checkbox]').attr('selected', !$('#' + el).attr('selected'))) {
+					return;
+				} else {
+					$('#' + el)[0].focus();
+				}
+			});
+		});
+	}
+	
   
   if(window.orientation != 0){
 	  document.body.className+=' landscape';
